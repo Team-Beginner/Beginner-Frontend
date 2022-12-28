a39d06574465d42d358d186f3bca91744a1565f8
Sanadevel <102668204+Sanadevel@users.noreply.github.com>
2022-12-28 16:58:31 +0900

    Forgotpassword 추가

--- FrontEnd/js/LoginPage.js
+++ FrontEnd/js/LoginPage.js
@@ -1,46 +1,47 @@
-const submitBtn = document.getElementById("submit");
-const emailInput = document.querySelector("#email");
-const pwInput = document.querySelector("#password");
+const submitBtn = document.getElementById('submit');
+const emailInput = document.querySelector('#email');
+const pwInput = document.querySelector('#password');
 
 const emailError = document.querySelector(".input-error[name='email-error']");
 const pwError = document.querySelector(".input-error[name='pw-error']");
 
-const loginForm = document.getElementById("login-form");
+const loginForm = document.getElementById('login-form');
 
 function submit(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  if (CheckEmail(emailInput.value) == false) {
-    emailError.setAttribute("id", "visible");
-    emailInput.style.borderColor = "#FF3737";
-    setTimeout(() => {
-      emailError.setAttribute("id", "hidden");
-      emailInput.style.borderColor = "#999999";
-    }, 3000);
-  }
-  if (pwInput.value == "") {
-    pwError.setAttribute("id", "visible");
-    pwInput.style.borderColor = "#FF3737";
-    setTimeout(() => {
-      pwError.setAttribute("id", "hidden");
-      pwInput.style.borderColor = "#999999";
-    }, 3000);
-  }
+	if (CheckEmail(emailInput.value) == false) {
+		emailError.setAttribute('id', 'visible');
+		emailInput.style.borderColor = '#FF3737';
+		setTimeout(() => {
+			emailError.setAttribute('id', 'hidden');
+			emailInput.style.borderColor = '#999999';
+		}, 3000);
+	} else if (pwInput.value == '') {
+		pwError.setAttribute('id', 'visible');
+		pwInput.style.borderColor = '#FF3737';
+		setTimeout(() => {
+			pwError.setAttribute('id', 'hidden');
+			pwInput.style.borderColor = '#999999';
+		}, 3000);
+	} else {
+		loginFetch();
+	}
 }
 
-loginForm.addEventListener("submit", submit);
+loginForm.addEventListener('submit', submit);
 
 function CheckEmail(str) {
-  var reg_email =
-    /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+	var reg_email =
+		/^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
-  if (!reg_email.test(str)) {
-    return false;
-  } else {
-    return true;
-  }
+	if (!reg_email.test(str)) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 // git pull origin master
-// git fetch origin 
+// git fetch origin
 // feature/{기능이름}
